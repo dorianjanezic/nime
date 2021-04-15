dc6e057a3090b2483db20ed9bb1db2a0b11978e7
Dorian Janezic <dtj5179@nyu.edu>
2021-04-15 23:11:30 +0200

    updated client side

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## [MQTT JavaScript client library](https://docs.emqx.io/en/broker/v4.3/development/javascript.html#mqtt-js-usage-example)
 
+## [NPM Mqtt example](https://www.npmjs.com/package/mqtt#example)
--- server/public/app.js
+++ server/public/app.js
@@ -1,34 +1,14 @@
-// const mqtt = require('mqtt')
-import mqtt from 'mqtt'
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://localhost:1883')
+console.log(client)
+ 
+client.on('connect', function () {
 
-// connection option
-const options = {
-  		clean: true, // retain session
-      connectTimeout: 4000, // Timeout period
-      // Authentication information
-      clientId: 'emqx_test',
-      username: 'emqx_test',
-      password: 'emqx_test',
-}
-
-// Connect string, and specify the connection method by the protocol
-// ws Unencrypted WebSocket connection
-// wss Encrypted WebSocket connection
-// mqtt Unencrypted TCP connection
-// mqtts Encrypted TCP connection
-// wxs WeChat applet connection
-// alis Alipay applet connection
-const connectUrl = 'wss://broker.emqx.io:8084/mqtt'
-const client = mqtt.connect(connectUrl, options)
-
-client.on('reconnect', (error) => {
-    console.log('reconnecting:', error)
+      client.publish('presence', 'Hello mqtt')
 })
-
-client.on('error', (error) => {
-    console.log('Connection failed:', error)
-})
-
-client.on('message', (topic, message) => {
-  console.log('receive message：', topic, message.toString())
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
 })
